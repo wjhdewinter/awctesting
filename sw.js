b4b87465f44f21f7859ed1a9ff0a9a19571cba8d
wjhdewinter <wjhdewinter@gmail.com>
2026-03-28 09:48:21 +0100

    sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "awc-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/Awcv1/",
+        "/Awcv1/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
