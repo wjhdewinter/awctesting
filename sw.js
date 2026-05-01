345a7701d29962f76846972ba6d6012edf4e1a01
wjhdewinter <wjhdewinter@gmail.com>
2026-05-01 21:18:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='awc-v25-pro-business';
+const CACHE='awc-v26-1-minimalistisch-pro';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon.png','./README.md'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
